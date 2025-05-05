d4e047d0cc0a7c08f286aa93855e2d0a4aeeeec1
Radiopandas <oliverhstacey@gmail.com>
2025-05-05 12:19:40 +1200

    Adds documentation to rotation.py

--- tetris/rotation.py
+++ tetris/rotation.py
@@ -30,16 +30,18 @@ height: int = 0
 var_defaults = [0, 0]
 
 def reset():
+    """Resets all necessary local global variables."""
     global width, height, var_defaults
     width, height = var_defaults
 
 def set_grid_size(_width: int, _height: int):
+    """Called from 'main.py' to tell 'rotation.py' the play area size."""
     global height, width
     height = _height
     width = _width
 
 def rotate_grid(grid, clockwise: bool = True):
-    """'Rotate' a grid by 90 degrees and return the result"""
+    """Rotates a grid by 90 degrees and return the result"""
     if clockwise:
         # Performs a matrix transpose
         rotated_grid = [[grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))]
@@ -60,34 +62,32 @@ def rotate_grid(grid, clockwise: bool = True):
 
 
 def get_wall_kicks(cur_rotation: int, direction: int, piece_type: int):
+    """Returns a list of wall kicks to be tried by rotate_tet()."""
     key = str(cur_rotation) + str((cur_rotation + direction) % 4)
+    # All pieces except the I piece use the same wall kicks
+    # so we only need to split the I pieces out.
     if piece_type == 1:
         return i_wall_kicks[key]
     else:
         return jlstz_wall_kicks[key]
 
 def check_rotation_validity(grid, rotated_grid, coord_grid, tetromino: Tetromino, offset: list = [0, 0]):
-    print("rotated_grid: ")
-    print(*rotated_grid, sep="\n")
+    """Checks if a piece can be rotated in a valid way using a given offset."""
 
     # Increments grid_coords by the offset
     grid_width: int = len(coord_grid)
-    print("Coord grid: ")
-    print(*coord_grid, sep="\n")
 
+    # Makes a deepcopy to avoid modifying the initial grid.
     _coord_grid = deepcopy(coord_grid)
-
+    # For every cell, applies the x and y offset.
     for y in range(grid_width):
         for x in range(grid_width):
             _coord_grid[y][x][0] += offset[0]
             _coord_grid[y][x][1] += offset[1]
-    print("New Coord grid: ")
-    print(*_coord_grid, sep="\n")
-    print("Oldish coord grid")
-    print(*coord_grid, sep="\n")
-
-    #sleep(3)
+    
     rotation_valid: bool = True
+    # For every cell in the new grid, checks that is in in a valid position,
+    # Meaning that it isn't out of bounds or occupied.
     for row_y, row in enumerate(_coord_grid):
         for row_x, cell in enumerate(row):
             x, y = cell[0], cell[1]
@@ -97,37 +97,40 @@ def check_rotation_validity(grid, rotated_grid, coord_grid, tetromino: Tetromino
                 break
 
             # Checks the new location is empty
-            if rotated_grid[row_y][row_x] and not [x, y] in tetromino.cells:
+            if rotated_grid[row_y][row_x] and [x, y] not in tetromino.cells:
                 if grid[y][x]:
                     print(f"{x}, {y} is invalid: Grid at {x}, {y}: {grid[y][x]}")
                     rotation_valid = False
                     break
+        
         if not rotation_valid:
             break
     
     return rotation_valid
 
 def rotate_tet(grid, focused_tet: Tetromino, cell_owners, clockwise: bool = True):
+    """Tries to rotate a piece, either where it is or with certain offsets."""
     # Since the 'o' piece can't be rotated, exits early
     if focused_tet.tet_type == 4:
         return
     
+    # Splits all the cells in 'focused_tet' into their x and y components.
     cur_cells = focused_tet.cells
-    
     x_coords: list[int] = []
     y_coords: list[int] = []
     for cell in cur_cells:
         x_coords.append(cell[0])
         y_coords.append(cell[1])
     
+    # Finds the furthest left and furthest up points of 'focused_tet'.
     min_x = min(x_coords)
     min_y = min(y_coords)
 
     x_offset: int = 0
     y_offset: int = 0
 
-    # Calculates how much offset is required to have each piece be
-    # Centred in the grids
+    # Calculates how much offset is required to have each piece
+    # be centred in the grids
     base_offset = 2 if focused_tet.tet_type == 1 else 1
     match focused_tet.times_rotated:
         case 0:
@@ -141,21 +144,24 @@ def rotate_tet(grid, focused_tet: Tetromino, cell_owners, clockwise: bool = True
     
     shape_width = max(get_range(x_coords), get_range(y_coords))
 
-    coord_grid: list = [[[x+min_x - x_offset, y+min_y - y_offset] for x in range(shape_width)] for y in range(shape_width)]
+    # Converts the inputted shape to a grid of bools and a grid of coordinates.
+    coord_grid: list = [
+        [[x+min_x - x_offset, y+min_y - y_offset] 
+        for x in range(shape_width)] 
+        for y in range(shape_width)]
 
-    # Converts the inputted shape to a grid of bools
     bool_grid = [[False for i in range(shape_width)] for j in range(shape_width)]
-
     for cell in cur_cells:
         x, y = cell[0] + x_offset, cell[1] + y_offset
         bool_grid[y - min_y][x - min_x] = True
-    
+    # Rotates the grid, either clockwise or anti-clockwise.
     rotated_grid = rotate_grid(bool_grid, clockwise)
+
 
     rotation_x_offset: int = 0
     rotation_y_offset: int = 0
     can_rotate: bool = True
-
+    # Checks the validity of the rotation with a variety of different offsets.
     direction: int = 1 if clockwise else -1
     wall_kicks = get_wall_kicks(focused_tet.times_rotated, direction, focused_tet.tet_type)
     for offset in wall_kicks:
@@ -165,8 +171,9 @@ def rotate_tet(grid, focused_tet: Tetromino, cell_owners, clockwise: bool = True
         if can_rotate:
             break
     
-
+    # If one of the offsets produced a valid rotation, rotates 'focused_tet'.
     if can_rotate:
+        # Clears the current cells of 'focused_tet'.
         for y, row in enumerate(bool_grid):
             for x, cell in enumerate(row):
                 if cell:
@@ -174,6 +181,7 @@ def rotate_tet(grid, focused_tet: Tetromino, cell_owners, clockwise: bool = True
                     grid[cur_y][cur_x] = False
                     cell_owners[cur_y][cur_x] = None
 
+        # Gets the new cells for 'focused_tet'.
         new_cells = []
         for y, row in enumerate(rotated_grid):
             for x, cell in enumerate(row):
@@ -182,6 +190,7 @@ def rotate_tet(grid, focused_tet: Tetromino, cell_owners, clockwise: bool = True
                     cur_y = coord_grid[y][x][1] + rotation_y_offset
                     new_cells.append([cur_x, cur_y])
         
+        # Applies the changes and updates the grids.
         focused_tet.cells = new_cells
         for cell in new_cells:
             x, y = cell[0], cell[1]
@@ -189,6 +198,7 @@ def rotate_tet(grid, focused_tet: Tetromino, cell_owners, clockwise: bool = True
             cell_owners[y][x] = focused_tet
                 
 
+        # Increments or decrements how many times the piece has been rotated.
         if clockwise:
             focused_tet.times_rotated += 1
             if focused_tet.times_rotated > 3:
